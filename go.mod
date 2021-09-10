bb51242e7414758b43d5b246ba48210dc911e7b7
Jeroen van Erp <jeroen@hierynomus.com>
2021-09-10 14:27:29 +0200

    Add username argument

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/hierynomus/iam-authorized-keys
 go 1.16
 
 require (
-	github.com/aws/aws-sdk-go v1.40.38
 	github.com/aws/aws-sdk-go-v2 v1.9.0
 	github.com/aws/aws-sdk-go-v2/config v1.7.0
 	github.com/aws/aws-sdk-go-v2/service/iam v1.9.0
--- main.go
+++ main.go
@@ -31,7 +31,6 @@ type UserKey struct {
 }
 
 type IAMKeyFetcher struct {
-	ctx    context.Context
 	config *Config
 	wg     *sync.WaitGroup
 
@@ -47,6 +46,8 @@ func (u *UserKey) String() string {
 const (
 	exitCodeOk    int = 0
 	exitCodeError int = 1
+
+	MaxPageSize = 1000
 )
 
 var (
@@ -55,7 +56,8 @@ var (
 
 	cmd = &cobra.Command{
 		Use:  "iam-authorized-keys",
-		RunE: FetchIAMUsers,
+		Args: cobra.MaximumNArgs(1),
+		RunE: FetchIAMSSHKeys,
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			switch verbosity {
 			case 0:
@@ -76,16 +78,22 @@ var (
 func main() {
 	ctx := context.Background()
 	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+
 	l := log.Output(os.Stderr)
 	ctx = l.WithContext(ctx)
+
 	cmd.Flags().StringVarP(&cfg.IAMGroup, "iam-group", "i", "", "Get SSH keys from this IAM group")
 	cmd.Flags().StringVarP(&cfg.AWSProfile, "aws-profile", "p", "", "Connect to the specified AWS profile")
 	cmd.Flags().CountVarP(&verbosity, "verbose", "v", "Print more verbose logging")
-	cmd.ExecuteContext(ctx)
+
+	if err := cmd.ExecuteContext(ctx); err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("Error occurred")
+		os.Exit(exitCodeError)
+	}
 }
 
 // The main function of the Root command
-func FetchIAMUsers(cmd *cobra.Command, args []string) error {
+func FetchIAMSSHKeys(cmd *cobra.Command, args []string) error {
 	// Handle SIGPIPE
 	//
 	// When sshd identifies a key in the stdout of this command, it closes
@@ -101,7 +109,6 @@ func FetchIAMUsers(cmd *cobra.Command, args []string) error {
 
 	ctx := cmd.Context()
 	f := &IAMKeyFetcher{
-		ctx:    ctx,
 		config: cfg,
 		wg:     &sync.WaitGroup{},
 		userCh: userCh,
@@ -111,11 +118,15 @@ func FetchIAMUsers(cmd *cobra.Command, args []string) error {
 
 	log.Info().Msg("Starting IAM Fetch")
 
-	go f.Fetch()
+	if len(args) == 1 {
+		go f.FetchPubKeyForUser(cmd.Context(), args[0])
+	} else {
+		go f.Fetch(cmd.Context())
+	}
 
 	for {
 		select {
-		case _ = <-c:
+		case <-c:
 			os.Exit(exitCodeOk)
 		case err := <-errCh:
 			log.Ctx(ctx).Error().Err(err).Msg("Error occurred")
@@ -128,43 +139,118 @@ func FetchIAMUsers(cmd *cobra.Command, args []string) error {
 	}
 }
 
-func (ikf *IAMKeyFetcher) Fetch() {
-	log.Ctx(ikf.ctx).Debug().Msg("Started IAM Key Fetcher")
-	awsConfig, err := config.LoadDefaultConfig(ikf.ctx)
-
+func (ikf *IAMKeyFetcher) Fetch(ctx context.Context) {
+	log.Ctx(ctx).Debug().Msg("Started IAM Key Fetcher")
+	iamClient, err := ikf.getIAMClient(ctx)
 	if err != nil {
 		ikf.errCh <- err
 		return
 	}
 
-	svc := iam.NewFromConfig(awsConfig)
-
-	users, err := ikf.listUsers(ikf.ctx, svc, cfg.IAMGroup)
+	users, err := ikf.listUsers(ctx, iamClient)
 	if err != nil {
 		ikf.errCh <- err
 		return
 	}
 
-	log.Ctx(ikf.ctx).Debug().Interface("users", users).Msg("Fetched users")
+	log.Ctx(ctx).Debug().Interface("users", users).Msg("Fetched users")
 
 	for _, u := range users {
 		ikf.wg.Add(1)
-		go ikf.fetchPubKey(ikf.ctx, svc, *u.UserName)
+		go ikf.fetchPubKey(ctx, iamClient, u)
 	}
 	ikf.wg.Wait()
 	ikf.doneCh <- true
 	close(ikf.userCh)
 }
 
-// Fetch the public key for the user
-func (ikf *IAMKeyFetcher) fetchPubKey(ctx context.Context, svc *iam.Client, user string) {
-	params := &iam.ListSSHPublicKeysInput{
-		UserName: &user,
+// FetchPubKeyForUser fetches the public key for the user specified on the command line
+func (ikf *IAMKeyFetcher) FetchPubKeyForUser(ctx context.Context, userName string) {
+	log.Ctx(ctx).Debug().Str("username", userName).Msg("Started IAM Key Fetcher")
+	iamClient, err := ikf.getIAMClient(ctx)
+	if err != nil {
+		ikf.errCh <- err
+		return
 	}
 
-	log.Ctx(ikf.ctx).Debug().Str("username", user).Msg("Fetching keys")
+	user, err := iamClient.GetUser(ctx, &iam.GetUserInput{
+		UserName: aws.String(userName),
+	})
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Str("username", userName).Msg("Failed to fetch user")
+		ikf.errCh <- err
+		return
+	}
 
-	if resp, err := svc.ListSSHPublicKeys(ikf.ctx, params); err == nil {
+	log.Ctx(ctx).Debug().Str("username", userName).Msg("Fetched user")
+
+	if user.User.UserName == nil {
+		log.Ctx(ctx).Error().Str("username", userName).Msg("User has no username")
+		return
+	}
+
+	if user.User.Arn == nil {
+		log.Ctx(ctx).Error().Str("username", userName).Msg("User has no ARN")
+		return
+	}
+
+	ikf.wg.Add(1) // fetchPubKey will decrement this
+	ikf.fetchPubKey(ctx, iamClient, *user.User)
+	ikf.doneCh <- true
+	close(ikf.userCh)
+}
+
+// getIAMClient returns a newly configured IAM client
+func (ikf *IAMKeyFetcher) getIAMClient(ctx context.Context) (*iam.Client, error) {
+	awsConfig, err := config.LoadDefaultConfig(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	svc := iam.NewFromConfig(awsConfig)
+	return svc, nil
+}
+
+// fetchPubKey fetches the public key for the user specified
+// func (ikf *IAMKeyFetcher) fetchPubKey(ctx context.Context, iamClient *iam.Client, userName string) {
+// 	defer ikf.wg.Done()
+// 	log.Ctx(ctx).Debug().Str("username", userName).Msg("Fetching user")
+// 	user, err := iamClient.GetUserRequest(&iam.GetUserInput{
+// 		UserName: aws.String(userName),
+// 	}).Send(ctx)
+// 	if err != nil {
+// 		log.Ctx(ctx).Error().Err(err).Str("username", userName).Msg("Failed to fetch user")
+// 		ikf.errCh <- err
+// 		return
+// 	}
+
+// 	log.Ctx(ctx).Debug().Str("username", userName).Msg("Fetched user")
+
+// 	if user.User.UserName == nil {
+// 		log.Ctx(ctx).Error().Str("username", userName).Msg("User has no username")
+// 		return
+// 	}
+
+// 	if user.User.Arn == nil {
+// 		log.Ctx(ctx).Error().Str("username", userName).Msg("User has no ARN")
+// 		return
+// 	}
+
+// 	log.Ctx(ctx).Debug().Str("username", userName).Msg("Fetching keys")
+// 	keys, err := iamClient.ListAccessKeysRequest(&iam.ListAccessKeysInput{
+// 		UserName: user.User.UserName,
+// 	}).Send(ctx)
+// 	if err != nil {
+// 		log.Ctx(ctx).Error().
+func (ikf *IAMKeyFetcher) fetchPubKey(ctx context.Context, svc *iam.Client, user types.User) {
+	params := &iam.ListSSHPublicKeysInput{
+		UserName: user.UserName,
+	}
+
+	log.Ctx(ctx).Debug().Str("username", *user.UserName).Str("arn", *user.Arn).Msg("Fetching keys")
+
+	if resp, err := svc.ListSSHPublicKeys(ctx, params); err == nil {
 		for _, k := range resp.SSHPublicKeys {
 			if k.Status != types.StatusTypeActive {
 				continue
@@ -173,31 +259,31 @@ func (ikf *IAMKeyFetcher) fetchPubKey(ctx context.Context, svc *iam.Client, user
 			params := &iam.GetSSHPublicKeyInput{
 				Encoding:       types.EncodingTypeSsh,
 				SSHPublicKeyId: k.SSHPublicKeyId,
-				UserName:       &user,
+				UserName:       user.UserName,
 			}
 
-			resp, err := svc.GetSSHPublicKey(ikf.ctx, params)
+			resp, err := svc.GetSSHPublicKey(ctx, params)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, err.Error())
+				log.Ctx(ctx).Error().Err(err).Str("username", *user.UserName).Str("ssh-key-id", *k.SSHPublicKeyId).Msg("Failed to fetch key")
 			}
 
 			ikf.userCh <- &UserKey{
-				Username: user,
+				Username: *user.UserName,
 				PubKey:   *resp.SSHPublicKey.Fingerprint,
 			}
 		}
 	} else {
-		fmt.Fprintln(os.Stderr, err.Error())
+		log.Ctx(ctx).Error().Err(err).Str("username", *user.UserName).Msg("Failed to list keys")
 	}
 	ikf.wg.Done()
 }
 
 // get all IAM users, or just those that are part of the defined group
-func (ikf *IAMKeyFetcher) listUsers(ctx context.Context, svc *iam.Client, iamGroup string) ([]types.User, error) {
-	if iamGroup != "" {
+func (ikf *IAMKeyFetcher) listUsers(ctx context.Context, svc *iam.Client) ([]types.User, error) {
+	if ikf.config.IAMGroup != "" {
 		params := &iam.GetGroupInput{
-			GroupName: aws.String(iamGroup),
-			MaxItems:  aws.Int32(1000),
+			GroupName: aws.String(ikf.config.IAMGroup),
+			MaxItems:  aws.Int32(MaxPageSize),
 		}
 
 		resp, err := svc.GetGroup(ctx, params)
@@ -209,7 +295,7 @@ func (ikf *IAMKeyFetcher) listUsers(ctx context.Context, svc *iam.Client, iamGro
 	}
 
 	params := &iam.ListUsersInput{
-		MaxItems: aws.Int32(1000),
+		MaxItems: aws.Int32(MaxPageSize),
 	}
 
 	resp, err := svc.ListUsers(ctx, params)
